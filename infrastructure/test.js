4150b2f656566965a40f23fbf1a6950f1e786b53
Wojciech Iskra <wojciech.iskra@schibsted.pl>
2016-03-17 11:02:11 +0100

    Added test env IAC

--- infrastructure/test.js
+++ infrastructure/test.js
@@ -3,13 +3,13 @@ var heroin = require('heroin-js');
 var configurator = heroin(process.env.HEROKU_API_TOKEN, {debug: false});
 
 var config = {
-    name: 'book-inventory-wiskra',
+    name: 'book-inventory-wiskra-test',
     region: 'eu',
     maintenance: false,
     stack: 'cedar-14',
     config_vars: {
         MONGOLAB_URI: process.env.MONGOLAB_URI,
-        name: "wiskra-prod"
+        name: "wiskra-test"
     },
     addons: {},
     collaborators: ['wojciech.iskra@schibsted.pl',
@@ -24,8 +24,7 @@ var config = {
         'http-end-to-end-continue': {enabled: false}
     },
     formation: [{process: 'web', quantity: 1, size: 'Free'}],
-    log_drains: [],
-    domains: ['book-inventory-wiskra.herokuapp.com']
+    log_drains: []
 };
 
 configurator(config);
