c6f77dbf290ec0d58c4516329b8e4a82cb1ea41f
Wojciech Iskra <wojciech.iskra@schibsted.pl>
2016-03-17 11:14:05 +0100

    Added test env IAC

--- infrastructure/prod.js
+++ infrastructure/prod.js
@@ -1,31 +1,13 @@
-var heroin = require('heroin-js');
+var _ = require('lodash');
+var configurator = require('./base').configurator;
 
-var configurator = heroin(process.env.HEROKU_API_TOKEN, {debug: false});
-
-var config = {
+var prod = {
     name: 'book-inventory-wiskra',
-    region: 'eu',
-    maintenance: false,
-    stack: 'cedar-14',
     config_vars: {
-        MONGOLAB_URI: process.env.MONGOLAB_URI,
         name: "wiskra-prod"
-    },
-    addons: {},
-    collaborators: ['wojciech.iskra@schibsted.pl',
-        'titzmateusz@gmail.com',
-        'kzych.loop@gmail.com'],
-    features: {
-        'runtime-dyno-metadata': {enabled: false},
-        'log-runtime-metrics': {enabled: false},
-        'http-session-affinity': {enabled: false},
-        preboot: {enabled: false},
-        'http-shard-header': {enabled: false},
-        'http-end-to-end-continue': {enabled: false}
-    },
-    formation: [{process: 'web', quantity: 1, size: 'Free'}],
-    log_drains: [],
-    domains: ['book-inventory-wiskra.herokuapp.com']
+    }
 };
 
+
+var config = _.merge({}, require('./base').config, prod);
 configurator(config);
--- infrastructure/test.js
+++ infrastructure/test.js
@@ -1,30 +1,13 @@
-var heroin = require('heroin-js');
+var _ = require('lodash');
+var configurator = require('./base').configurator;
 
-var configurator = heroin(process.env.HEROKU_API_TOKEN, {debug: false});
-
-var config = {
+var prod = {
     name: 'book-inventory-wiskra-test',
-    region: 'eu',
-    maintenance: false,
-    stack: 'cedar-14',
     config_vars: {
-        MONGOLAB_URI: process.env.MONGOLAB_URI,
         name: "wiskra-test"
-    },
-    addons: {},
-    collaborators: ['wojciech.iskra@schibsted.pl',
-        'titzmateusz@gmail.com',
-        'kzych.loop@gmail.com'],
-    features: {
-        'runtime-dyno-metadata': {enabled: false},
-        'log-runtime-metrics': {enabled: false},
-        'http-session-affinity': {enabled: false},
-        preboot: {enabled: false},
-        'http-shard-header': {enabled: false},
-        'http-end-to-end-continue': {enabled: false}
-    },
-    formation: [{process: 'web', quantity: 1, size: 'Free'}],
-    log_drains: []
+    }
 };
 
+
+var config = _.merge({}, require('./base').config, prod);
 configurator(config);
